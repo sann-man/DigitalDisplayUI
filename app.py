1c1902ff96c0b3804e27f8c325bf7a58713449a4
sann-man <rahsaanyj@gmail.com>
2024-11-20 13:52:22 -0800

    added basic login page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home(): 
     return render_template('main.html')
 
+@app.route('/login')  # Fixed: Added the leading slash
+def login(): 
+    return render_template('login.html')
 
 if __name__ == "__main__": 
     app.run(debug=True)
